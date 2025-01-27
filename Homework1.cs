ec264730d7f8484bde667686c59d8a4228cda1eb
Jab657 <132285328+Jab657@users.noreply.github.com>
2025-01-27 00:39:56 -0600

    Add files via upload
    Added Homework 1

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Code for Homework1
+        double x = 2.5;
+        double y = 3.3;
+
+        double z = 4*x*x + 3*y;
+
+        Console.WriteLine("The value of z is " + z);
+
+    }
+}
